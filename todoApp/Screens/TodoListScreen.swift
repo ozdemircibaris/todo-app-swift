3c6dad78ff8e22ab7dd809590ab7620a84589caa
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-12-06 12:46:16 +0300

    building app structure

---
+++ todoApp/Screens/TodoListScreen.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListScreen.swift
+//  todoApp
+//
+//  Created by 10lift on 6.12.2022.
+//
+
+import SwiftUI
+
+struct TodoListScreen: View {
+    var body: some View {
+        Text("Hello, todo`s!")
+    }
+}
+
+struct TodoListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListScreen()
+    }
+}
