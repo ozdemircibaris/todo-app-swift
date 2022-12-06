31c580c56d3be4593c2b8a435823d4461e6ba44e
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-12-06 12:32:45 +0300

    project initialized

---
+++ todoApp/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by 10lift on 6.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
