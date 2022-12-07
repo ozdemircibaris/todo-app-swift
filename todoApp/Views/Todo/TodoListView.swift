6d328046b2d8a56f58d32e5edf3eef8f48abe982
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-12-07 17:55:25 +0300

    add todo list view

--- todoApp/Views/Todo/TodoListView.swift
+++ todoApp/Views/Todo/TodoListView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct TodoListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @EnvironmentObject var todoListVM: TodoListViewModel
 
-struct TodoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        TodoListView()
+    func deleteTodo(todoId: String) {
+        todoListVM.deleteTodo(todoId: todoId)
+    }
+    
+    func duplicateTodo(todoId: String) {
+        todoListVM.duplicateTodo(todoId: todoId)
+    }
+
+    var body: some View {
+        VStack {
+            ForEach($todoListVM.todoList) { $todo in
+                HStack {
+                    // completed
+                    Button(action:
+                            {
+                        todoListVM.completeTodo(todoId: todo.id, completed: !todo.completed)
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 1, style: .continuous)
+                                .fill(todo.completed ? Color.purple:Color.black)
+                                .frame(width: 20, height: 20)
+                                .border(Color.purple, width: 2).cornerRadius(4)
+                            if todo.completed {
+                                Image(systemName: "checkmark").zIndex(1)
+                            }
+                        }.frame(width: 20, height: 20)
+                    }.foregroundColor(Color.white)
+                        .buttonStyle(PlainButtonStyle())
+                    // title
+                    VStack {
+                        TextField("Type title", text: $todo.title)
+                            .textFieldStyle(PlainTextFieldStyle())
+
+                        Divider()
+                            .frame(height: 1)
+                            .padding(.horizontal, 30)
+                            .background(Color.gray)
+                    }
+
+                    Menu("") {
+                        Button("Delete") {
+                            deleteTodo(todoId: todo.id)
+                        }
+                        Button("Duplicate", action: {
+                            duplicateTodo(todoId: todo.id)
+                        })
+                    }.frame(width: 100)
+                }
+            }
+        }
     }
 }
