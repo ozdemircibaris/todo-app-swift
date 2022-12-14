a7169c687b7c3cd7817f4adee593a3e879eae35d
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-12-14 16:27:30 +0300

    disable list background color

--- todoApp/Views/Todo/TodoListView.swift
+++ todoApp/Views/Todo/TodoListView.swift
@@ -12,25 +12,38 @@ enum FocusField: Hashable {
     case row(index: Int)
 }
 
+extension NSTableView {
+    open override func viewDidMoveToWindow() {
+        super.viewDidMoveToWindow()
+        
+        backgroundColor = NSColor.clear
+        if let esv = enclosingScrollView {
+            esv.drawsBackground = false
+        }
+    }
+}
+
 struct TodoListView: View {
-    @EnvironmentObject var todoListVM: TodoListViewModel
-    @State var isHover: Bool = false
     @FocusState private var focusedField: FocusField?
-    @State var focusedIndex: Int = 0
-    
-    
+    @EnvironmentObject var todoListVM: TodoListViewModel
+
+    @State var todoDetail: TodoViewModel?
+    @State var isHover: Bool = false
+    @State private var todoDetailModalStatus: Bool = false
+
+
     func deleteTodo(todoId: String) {
         todoListVM.deleteTodo(todoId: todoId)
     }
-    
+
     func duplicateTodo(todoId: String) {
         todoListVM.duplicateTodo(todoId: todoId)
     }
-    
+
     func onMove(from source: IndexSet, to destination: Int) {
         todoListVM.moveTodo(source: source, destionation: destination)
     }
-    
+
     func handleNextTextField(todoIndex: Int) {
         // get the current index
         // current index + 1 and check the next item avability
@@ -43,69 +56,94 @@ struct TodoListView: View {
             focusedField = .row(index: nextItemIndex)
         }
     }
-    
-    var body: some View {
-        VStack {
-            List {
-                ForEach(todoListVM.todoList.indices, id: \.self) { index in
-                    let todo = todoListVM.todoList[index];
 
-                    HStack {
-                        Image(systemName: "line.3.horizontal")
-                            .onHover { hover in
-                                isHover = hover
-                            }
-                        // completed
-                        Button(action:
-                                {
-                            todoListVM.completeTodo(todoId: todo.id, completed: !todo.completed)
-                        }) {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 1, style: .continuous)
-                                    .fill(todo.completed ? Color.purple:Color.black)
-                                    .frame(width: 20, height: 20)
-                                    .border(Color.purple, width: 2).cornerRadius(4)
-                                if todo.completed {
-                                    Image(systemName: "checkmark").zIndex(1)
-                                }
-                            }.frame(width: 20, height: 20)
-                        }.foregroundColor(Color.white)
-                            .buttonStyle(PlainButtonStyle())
+    var body: some View {
+        HStack {
+            // todo list
+            VStack {
+                List {
+                    ForEach(todoListVM.todoList.indices, id: \.self) { index in
+                        let todo = todoListVM.todoList[index];
                         
-                        // title
-                        VStack {
-                            TextField("Type title", text: self.$todoListVM.todoList[index].title)
-                                .textFieldStyle(PlainTextFieldStyle())
-                                .focused($focusedField, equals: .row(index: index))
-                                .onSubmit {
-                                    handleNextTextField(todoIndex: index)
+                        HStack {
+                            Image(systemName: "line.3.horizontal")
+                                .onHover { hover in
+                                    isHover = hover
                                 }
+                            // completed
+                            Button(action:
+                                    {
+                                todoListVM.completeTodo(todoId: todo.id, completed: !todo.completed)
+                            }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 1, style: .continuous)
+                                        .fill(todo.completed ? Color.purple:Color.black)
+                                        .frame(width: 20, height: 20)
+                                        .border(Color.purple, width: 2).cornerRadius(4)
+                                    if todo.completed {
+                                        Image(systemName: "checkmark").zIndex(1)
+                                    }
+                                }.frame(width: 20, height: 20)
+                            }.foregroundColor(Color.white)
+                                .buttonStyle(PlainButtonStyle())
                             
-                            Divider()
-                                .frame(height: 1)
-                                .padding(.horizontal, 30)
-                                .background(Color.gray)
-                        }
-                        
-                        // action button
-                        Menu {
-                            Button("Delete") {
-                                deleteTodo(todoId: todo.id)
+                            // title
+                            VStack {
+                                TextField("Type title", text: self.$todoListVM.todoList[index].title)
+                                    .textFieldStyle(PlainTextFieldStyle())
+                                    .focused($focusedField, equals: .row(index: index))
+                                    .onSubmit {
+                                        handleNextTextField(todoIndex: index)
+                                    }
+                                
+                                Divider()
+                                    .frame(height: 1)
+                                    .padding(.horizontal, 30)
+                                    .background(Color.gray)
                             }
-                            Button("Duplicate", action: {
-                                duplicateTodo(todoId: todo.id)
-                            })
-                        } label: {
-                            Image(systemName: "bookmark.circle")
-                                .resizable()
-                                .frame(width:24.0, height: 24.0)
-                        }.frame(width: 30, alignment: .center)
-                            .menuIndicator(.hidden)
-                            .moveDisabled(true)
+                            
+                            // action button
+                            Menu {
+                                Button("Detail") {
+                                    withAnimation {
+                                        todoDetailModalStatus = true
+                                    }
+                                    todoDetail = todo
+                                    
+                                }
+                                Button("Delete") {
+                                    deleteTodo(todoId: todo.id)
+                                }
+                                Button("Duplicate", action: {
+                                    duplicateTodo(todoId: todo.id)
+                                })
+                            } label: {
+                                Image(systemName: "bookmark.circle")
+                                    .resizable()
+                                    .frame(width:24.0, height: 24.0)
+                            }.frame(width: 30, alignment: .center)
+                                .menuIndicator(.hidden)
+                                .moveDisabled(true)
+                        }
+                    }.onMove(perform: onMove)
+                        .moveDisabled(!isHover)
+                }
+            }
+            
+            // todo detail
+            if todoDetailModalStatus {
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button(action: {withAnimation {
+                            todoDetailModalStatus = false
+                        }}) {
+                            Image(systemName: "x.circle")
+                        }.buttonStyle(.plain)
                     }
-                    
-                }.onMove(perform: onMove)
-                    .moveDisabled(!isHover)
+                    Text("\(self.todoDetail?.title ?? "")").font(.title)
+                    Spacer()
+                }.frame(width: 400)
             }
         }
     }
